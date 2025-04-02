36aad79820faf147a24c428e06f25247c29c720a
George Barnick <george@fxbgtech.com>
2025-04-02 18:23:45 -0400

    Python2 -> Python3 and refactored threading to improve efficiency
    Refactored how threading is handled to only run SSH connections on IPs that respond to TCP/22 first. Use socket to check if port 22 is open before threading SSH connections, rather than threading every single IP on the subnet and waiting for timeouts before the script completes.
    
    Also minor tweak to format SSH response output in UTF-8 instead of raw unformatted text for better legibility of the response output.

--- main.py
+++ main.py
@@ -1,127 +1,103 @@
 import threading
-from Queue import Queue
+from queue import Queue
 import paramiko
+import socket
+
 url = ''
-which_command = raw_input("Which mode? set-inform, info, or set-default [info]: ")
+which_command = input("Which mode? set-inform, info, or set-default [info]: ")
 if which_command == "set-inform":
-    url = raw_input("Enter your set-inform URL [http://p01.hostifi.net:8080/inform]: ")
-username_ = raw_input("Enter device username [ubnt]: ")
-password_ = raw_input("Enter device password [ubnt]: ")
-subnet_ = raw_input("Enter subnet [192.168.1.0]: ")
+    url = input("Enter your set-inform URL [http://unifi.fxbgtech.com:8080/inform]: ")
+username_ = input("Enter device username [ubnt]: ")
+password_ = input("Enter device password [ubnt]: ")
+subnet_ = input("Enter subnet [192.168.1.0]: ")
+#TODO: Accept CIDR notation to enable different subnet sizes
+#TODO: Attempt to read the primary network adapter's current subnet to default to that
 
-if url != '':
-    url = url
-else:
-    url = "http://p01.hostifi.net:8080/inform"
-
-if which_command != '':
-    which_command = which_command
-else:
+if url == '':
+    url = "http://unifi.fxbgtech.com:8080/inform"
+if which_command == '':
     which_command = 'info'
-
-if username_ != '':
-    USERNAME = username_
-else:
+if username_ == '':
     USERNAME = 'ubnt'
-
-if password_ != '':
-    PASSWORD = password_
 else:
+    USERNAME = username_
+if password_ == '':
     PASSWORD = 'ubnt'
-
-if subnet_ != '':
-    SUBNET = subnet_
 else:
+    PASSWORD = password_
+if subnet_ == '':
     SUBNET = '192.168.1.0'
+else:
+    SUBNET = subnet_
 
 SUBNET = SUBNET[:-1]
 PORT = 22
 COMMAND = url
+hostnames = [SUBNET + str(i) for i in range(1, 254)]
+responsive_ips = []
 
-hostnames = []
-ips = range(1,254)
-for i in ips:
-    hostnames.append(SUBNET + str(i))
+def is_port_open(hostname):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(1)  # Set timeout to 1 second
+    result = sock.connect_ex((hostname, PORT))
+    sock.close()
+    if result == 0:
+        responsive_ips.append(hostname)
+
+def execute_command(hostname, command, output_q):
+    print(f"Starting... {hostname}")
+    try:
+        client = paramiko.SSHClient()
+        client.load_system_host_keys()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(hostname, port=PORT, username=USERNAME, password=PASSWORD)
+        stdin, stdout, stderr = client.exec_command(command)
+        output = stdout.read().decode('utf-8')
+        output_q.put((hostname, output))
+    except Exception as e:
+        output_q.put((hostname, str(e)))
+    finally:
+        client.close()
 
 def info(hostname, output_q):
-    print "Starting..."
-    print hostname
-    try:
-        client = paramiko.SSHClient()
-        client.load_system_host_keys()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-        client.connect(hostname, port=PORT, username=USERNAME, password=PASSWORD)
-
-        stdin, stdout, stderr = client.exec_command("mca-cli <<EOF\ninfo\nquit\nEOF")
-        print stdout.read()
-    except:
-        client.close()
-    finally:
-        client.close()
+    execute_command(hostname, "mca-cli <<EOF\ninfo\nquit\nEOF", output_q)
 
 def set_default(hostname, output_q):
-    print "Starting..."
-    print hostname
-    try:
-        client = paramiko.SSHClient()
-        client.load_system_host_keys()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    execute_command(hostname, "mca-cli <<EOF\nset-default\nquit\nEOF", output_q)
 
-        client.connect(hostname, port=PORT, username=USERNAME, password=PASSWORD)
-
-        stdin, stdout, stderr = client.exec_command("mca-cli <<EOF\nset-default\nquit\nEOF")
-        print stdout.read()
-    except:
-        client.close()
-    finally:
-        client.close()
 def set_inform(hostname, output_q):
-    print "Starting..."
-    print hostname
-    try:
-        client = paramiko.SSHClient()
-        client.load_system_host_keys()
-        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
-        client.connect(hostname, port=PORT, username=USERNAME, password=PASSWORD)
-
-        stdin, stdout, stderr = client.exec_command("mca-cli <<EOF\nset-inform %s\nquit\nEOF" % (COMMAND))
-        print stdout.read()
-    except:
-        client.close()
-    finally:
-        client.close()
-
+    execute_command(hostname, f"mca-cli <<EOF\nset-inform {COMMAND}\nquit\nEOF", output_q)
 
 if __name__ == "__main__":
-
     output_q = Queue()
-    try:
-        # Start thread for each router in routers list
-        for hostname in hostnames:
-            if which_command == "set-inform":
-                my_thread = threading.Thread(target=set_inform, args=(hostname, output_q))
-            if which_command == "info":
-                my_thread = threading.Thread(target=info, args=(hostname, output_q))
-            if which_command == "set-default":
-                my_thread = threading.Thread(target=set_default, args=(hostname, output_q))
-            my_thread.start()
+    scan_threads = []
 
-        # Wait for all threads to complete
-        main_thread = threading.currentThread()
-        for some_thread in threading.enumerate():
-            if some_thread != main_thread:
-                some_thread.join()
+    print("Scanning subnet for IPs that respond on port 22...")
+    for hostname in hostnames:
+        scan_thread = threading.Thread(target=is_port_open, args=(hostname,))
+        scan_threads.append(scan_thread)
+        scan_thread.start()
 
-        # Retrieve everything off the queue - k is the router IP, v is output
-        # You could also write this to a file, or create a file for each router
-    except:
-        pass
+    for thread in scan_threads:
+        thread.join()
+
+    print("Beginning SSH connections to responsive IPs...")
+    ssh_threads = []
+    for hostname in responsive_ips:
+        if which_command == "set-inform":
+            my_thread = threading.Thread(target=set_inform, args=(hostname, output_q))
+        elif which_command == "info":
+            my_thread = threading.Thread(target=info, args=(hostname, output_q))
+        elif which_command == "set-default":
+            my_thread = threading.Thread(target=set_default, args=(hostname, output_q))
+        ssh_threads.append(my_thread)
+        my_thread.start()
+
+    for thread in ssh_threads:
+        thread.join()
+
     while not output_q.empty():
-        my_dict = output_q.get()
-        for k, val in my_dict.iteritems():
-            print k
-            print val
+        hostname, output = output_q.get()
+        print(f"{hostname}:\n{output}")
 
-wait = raw_input("Press any key to exit...")
+    input("Press any key to exit...")
